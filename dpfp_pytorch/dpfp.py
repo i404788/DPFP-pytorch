e4cdd53af904e4a9cd7c4cca8d90e28178b2e928
Casper <casper@devdroplets.com>
2021-02-26 21:16:06 +0000

    More cleaning

--- dpfp_pytorch/dpfp.py
+++ dpfp_pytorch/dpfp.py
@@ -3,15 +3,6 @@ import torch.nn as nn
 from torch import cat
 from torch.nn.functional import relu
 
-def exists(val):
-    return val is not None
-
-def empty(tensor):
-    return tensor.numel() == 0
-
-def default(val, d):
-    return val if exists(val) else d
-
 def dpfp(x, nu=1):
     x = cat([relu(x), relu(-x)], dim=-1)
     x_rolled = cat([x.roll(shifts=j, dims=-1) for j in range(1, nu+1)], dim=-1)
@@ -19,10 +10,9 @@ def dpfp(x, nu=1):
     return x_repeat*x_rolled
 
 class DPFPCell(nn.Module):
-    def __init__(self, dim, heads = 8, dim_head=64, nu=1, dropout=0.):
+    def __init__(self, dim, heads = 8, nu=1, dropout=0.):
         super().__init__()
         assert dim % heads == 0, 'dimension must be divisible by number of heads'
-        dim_head = default(dim_head, dim // heads)
         inner_dim = 2 * dim * nu
 
         self.nu = nu
@@ -30,9 +20,7 @@ class DPFPCell(nn.Module):
         self.to_k = nn.Linear(dim, dim)
         self.to_v = nn.Linear(dim, inner_dim)
         self.to_out = nn.Linear(inner_dim, dim)
-        
         self.to_beta = nn.Linear(dim, 1)
-
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, W, context=None):
@@ -52,8 +40,11 @@ class DPFPCell(nn.Module):
         kp = dpfp(k, nu=self.nu)
         W = W + torch.einsum('bp, bq->bpq', (dv, kp))
 
-        # y(i)=W(i)φ(q(i)
-        return self.to_out(torch.einsum('bij, bi -> bi', W, dpfp(q, nu=self.nu))), W
+        # y(i)=W(i)φ(q(i))
+        out = torch.einsum('bij, bi -> bi', W, dpfp(q, nu=self.nu))
+        out = self.to_out(out)
+        out = self.dropout(out)
+        return out, W
             
 
 
